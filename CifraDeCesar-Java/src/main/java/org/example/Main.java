460a7b465c4e9cfbc193f3244483d7740c91eb34
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-13 19:10:37 -0300

    criar dialog para pedir rotacao

--- CifraDeCesar-Java/src/main/java/org/example/Main.java
+++ CifraDeCesar-Java/src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
+    static String texto_original;
+    static int rotacao = 0;
     public static void main(String[] args) {
+
         JFrame mainFrame = new JFrame("Cifra de Cesar");
         mainFrame.setSize(490, 464);
 
@@ -27,6 +32,36 @@ public class Main {
         output.setRows(8);
         output.setColumns(38);
         areaOutput.add(output);
+
+        // Criar janela para pedir valor de rotação
+        JFrame dialogFrame = new JFrame("Rotacao");
+        JDialog rotacaoDialog = new JDialog(dialogFrame);
+        rotacaoDialog.setSize(200, 130);
+        JTextField rotacaoInput = new JTextField();
+        JButton confirmarBotao = new JButton("Confirmar");
+        rotacaoDialog.add(rotacaoInput);
+        rotacaoDialog.add(confirmarBotao);
+        rotacaoDialog.setLayout(null);
+        rotacaoInput.setBounds(50,0,100, 30);
+        confirmarBotao.setBounds(50, 50, 100, 50);
+
+        // Abrir dialog da rotação quando clicar em encriptar
+        encriptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rotacaoDialog.setVisible(true);
+            }
+        });
+
+        // Fechar dialog da rotação quando clicar em confirmar
+        confirmarBotao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rotacao = Integer.parseInt(rotacaoInput.getText());
+                rotacaoDialog.setVisible(false);
+            }
+        });
+
 
         areaInput.setBounds(20,50,450,100);
         areaBotoes.setBounds(100, 190, 300, 50);
