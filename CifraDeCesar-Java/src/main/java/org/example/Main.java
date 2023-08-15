b4536c24724a6643508dab10dd9408875e028df0
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-15 17:15:54 -0300

    adicionar funcionalidade de encriptar mensagem

--- CifraDeCesar-Java/src/main/java/org/example/Main.java
+++ CifraDeCesar-Java/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-    static String texto_original;
+    static String texto_original, texto_encriptado;
     static int rotacao = 0;
     public static void main(String[] args) {
 
@@ -45,23 +45,25 @@ public class Main {
         rotacaoInput.setBounds(50,0,100, 30);
         confirmarBotao.setBounds(50, 50, 100, 50);
 
-        // Abrir dialog da rotação quando clicar em encriptar
+        // Guardar string original e abrir dialog da rotação quando clicar em encriptar
         encriptar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                texto_original = input.getText();
                 rotacaoDialog.setVisible(true);
             }
         });
 
-        // Fechar dialog da rotação quando clicar em confirmar
+        // Fechar dialog da rotação e guardar valor da rotação quando clicar em confirmar
         confirmarBotao.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 rotacao = Integer.parseInt(rotacaoInput.getText());
                 rotacaoDialog.setVisible(false);
+                encriptar();
+                output.setText(texto_encriptado);
             }
         });
-
 
         areaInput.setBounds(20,50,450,100);
         areaBotoes.setBounds(100, 190, 300, 50);
@@ -71,5 +73,16 @@ public class Main {
         mainFrame.add(areaOutput);
         mainFrame.setLayout(null);
         mainFrame.setVisible(true);
+    }
+
+    public static void encriptar() {
+        texto_encriptado = "";
+
+        for (int i = 0; i < texto_original.length(); i++) {
+            int char_original = texto_original.charAt(i);
+            char char_encriptado = (char) (char_original + rotacao);
+
+            texto_encriptado = texto_encriptado + char_encriptado;
+        }
     }
 }
